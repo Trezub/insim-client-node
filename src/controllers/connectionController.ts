fc628880a48db9327f6b4962c2ee5c1e2d0c2953
Adriano Trezub Déa <adriano.trezub@gmail.com>
2021-08-10 17:49:21 -0300

    User model

--- src/controllers/connectionController.ts
+++ src/controllers/connectionController.ts
@@ -1,4 +1,6 @@
+import { getRepository } from 'typeorm';
 import Connection from '../Connection';
+import { User } from '../database/models/User';
 import { PlayerCar } from '../enums/PlayerCar';
 import inSimClient from '../inSimClient';
 import log from '../log';
@@ -8,6 +10,7 @@ import { PlayerRenameProps } from '../packets/IS_CPR';
 import { NewConnectionInfoProps } from '../packets/IS_NCI';
 import { NewConnectionProps } from '../packets/IS_NCN';
 import IS_PLC from '../packets/IS_PLC';
+import IS_TINY, { TinyPacketSubType } from '../packets/IS_TINY';
 
 class ConnectionController {
     connections = new Map<number, Connection>();
@@ -24,6 +27,14 @@ class ConnectionController {
             connection.connectionId,
             new Connection(connection),
         );
+        if (connection.requestId !== 255) {
+            await inSimClient.sendPacket(
+                IS_TINY.fromProps({
+                    requestId: 255,
+                    subType: TinyPacketSubType.TINY_NCI,
+                }),
+            );
+        }
         // await inSimClient.sendPacket(
         //     IS_PLC.fromProps({
         //         connectionId: connection.connectionId,
@@ -32,7 +43,10 @@ class ConnectionController {
         // );
     }
 
-    handleConnectionLeave({ connectionId, reason }: ConnectionLeaveProps) {
+    async handleConnectionLeave({
+        connectionId,
+        reason,
+    }: ConnectionLeaveProps) {
         const connection = this.connections.get(connectionId);
         if (!connection) {
             log.error(`Connection ${connectionId} not found.`);
@@ -40,6 +54,16 @@ class ConnectionController {
         log.info(
             `Connection left: ${connection.username} (${connection.id}). Reason: ${ConnectionLeaveReason[reason]}`,
         );
+        const userRepository = getRepository(User);
+        await userRepository.save({
+            id: connection.userId,
+            cars: connection.cars,
+            cash: connection.cash,
+            health: connection.health,
+            language: connection.language,
+            lastIPAddress: connection.ipAddress,
+            username: connection.username,
+        });
         this.connections.delete(connectionId);
     }
 
@@ -55,20 +79,39 @@ class ConnectionController {
         connection.nickname = nickname;
     }
 
-    handleConnectionInfo({
+    async handleConnectionInfo({
         connectionId,
         ipAddress,
         language,
         requestId,
         userId,
     }: NewConnectionInfoProps) {
+        // log.debug({ connectionId, ipAddress, language, requestId, userId });
         const connection = this.connections.get(connectionId);
         if (!connection) {
             log.error(`Connection ${connectionId} not found.`);
         }
+        const userRepository = getRepository(User);
+
+        let user =
+            (await userRepository.findOne(userId)) ||
+            userRepository.create({
+                id: userId,
+            });
+
+        const { username } = connection;
+        user.lastIPAddress = ipAddress;
+        user.username = username;
+        user.language = language;
+
+        user = await userRepository.save(user);
+
         connection.ipAddress = ipAddress;
         connection.language = language;
         connection.userId = userId;
+        connection.cars = user.cars;
+        connection.cash = user.cash;
+        connection.health = user.health;
     }
 }
 
---
+++ src/database/models/User.ts
@@ -0,0 +1,39 @@
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    PrimaryColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { Language } from '../../enums/Languages';
+import { PlayerCar } from '../../Player';
+
+@Entity()
+export class User {
+    @PrimaryColumn({ unique: true })
+    id: number;
+
+    @CreateDateColumn()
+    createdAt: number;
+
+    @UpdateDateColumn()
+    updatedAt: number;
+
+    @Column({ default: 0 })
+    cash: number;
+
+    @Column({ default: 100 })
+    health: number;
+
+    @Column({ type: 'simple-array', default: 'UF1' })
+    cars: PlayerCar[];
+
+    @Column({ unique: true })
+    username: string;
+
+    @Column({ type: 'text' })
+    lastIPAddress: string;
+
+    @Column({ enum: Language })
+    language: number;
+}
