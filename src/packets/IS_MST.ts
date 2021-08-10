3599fe048d2eec798e089d06f5285c2e22022143
Adriano Trezub Déa <adriano.trezub@gmail.com>
2021-08-10 14:26:11 -0300

    New Packer: IS_MST

---
+++ src/packets/IS_MST.ts
@@ -0,0 +1,14 @@
+import { PacketType } from '../enums/PacketType';
+import toByteArray from '../utils/toByteArray';
+
+export default {
+    fromProps(message: string) {
+        return Buffer.from([
+            68,
+            PacketType.ISP_MST,
+            0,
+            0,
+            ...toByteArray(message, 64),
+        ]);
+    },
+};
